90bb2c8ebdfa509bc3f1eebf36790e80820717a4
Allison Larson <allisonlarson91@gmail.com>
2021-12-01 05:51:28 +0000

    Day 1: Make inputs integers

--- 1.rb
+++ 1.rb
@@ -1,9 +1,9 @@
-data = File.read("inputs/input1.txt").split
-puts data.each_cons(2).inject(0) { |s, x| x[0].to_i < x[1].to_i ? s+=1 : s }
+data = File.read("inputs/input1.txt").split.collect!(&:to_i)
+puts data.each_cons(2).inject(0) { |s, x| x[0] < x[1] ? s+=1 : s }
 
 prev = 0
 puts data.each_cons(3).inject(0) { |s, x| 
-    current = x[0].to_i + x[1].to_i + x[2].to_i 
+    current = x.inject(:+)
     if prev != 0 && prev < current
         s+=1
     end
