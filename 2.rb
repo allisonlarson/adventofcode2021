c4ffa7ba5c2d8a0f6185621125bce3879acbf7bc
Allison Larson <allisonlarson91@gmail.com>
2021-12-02 05:31:18 +0000

    Day2: Use inject, use elsif's

--- 2.rb
+++ 2.rb
@@ -1,18 +1,14 @@
-data = File.read("inputs/input2.txt").lines
-x = 0
-y = 0
-a = 0 
-data.each { |instruction| 
+data = File.read("inputs/data.txt").lines
+totals = data.inject(Hash.new(0)) { |axis, instruction| 
     direction,movement = instruction.split
     if direction == "up"
-        a -= movement.to_i
+        axis[:a] -= movement.to_i
+    elsif direction == "down"
+        axis[:a] += movement.to_i
+    elsif direction == "forward"
+        axis[:y] += movement.to_i
+        axis[:x] += (axis[:a] * movement.to_i)
     end
-    if direction == "down"
-        a += movement.to_i
-    end
-    if direction == "forward"
-        y += movement.to_i
-        x += (a * movement.to_i)
-    end
+    axis
 }
-puts x * y
+puts totals[:x] * totals[:y]
