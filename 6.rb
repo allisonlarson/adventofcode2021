3d1e1e8ae1809c4a3ffbfd3d8695c03bd0acc209
Allison Larson <allisonlarson91@gmail.com>
2021-12-05 22:09:46 -0800

    Day 6 pt2, Optimized, but less cute x2

--- 6.rb
+++ 6.rb
@@ -1,23 +1,20 @@
-
 class Day6
     def initialize(filename)
         data = File.read(filename).split(",").map(&:to_i)
-        @school = Hash.new(0)
-        data.each { |f| @school[f] += 1 }
+        @school = data.tally
     end
 
     def run(day_count)
         day_count.to_i.times do 
-            new_school = {}
-            new_school[0] = @school[1]
-            new_school[1] = @school[2]
-            new_school[2] = @school[3]
-            new_school[3] = @school[4]
-            new_school[4] = @school[5]
-            new_school[5] = @school[6] 
-            new_school[6] = @school[7] + @school[0]
-            new_school[7] = @school[8]
-            new_school[8] = @school[0]
+            new_school = Hash.new(0)
+            @school.each do |d, f|
+                if d == 0
+                    new_school[6] += f
+                    new_school[8] += f
+                else
+                    new_school[d-1] += f
+                end
+            end
             @school = new_school
         end
         @school.values.sum
